2ece39b311fa96e34374d61aa23711c5bfce09c9
Gagan Deep Singh <me@gagan.pro>
2017-05-15 19:57:55 +0530

    Added PHP Unit Testing with newly created docker image at gagan0123/php:7.0 and a sample test case

---
+++ tests/test-barebone.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class SampleTest
+ *
+ * @package Barebone
+ */
+
+/**
+ * Sample test case.
+ */
+class BareBone_Tests extends WP_UnitTestCase {
+
+	private $barebone;
+
+	/**
+	 * Setting up the test set
+	 */
+	function setUp() {
+		$this->barebone			 = Barebone::get_instance();
+	}
+
+	/**
+	 * A single example test.
+	 */
+	function test_get_option() {
+		$option	 = $this->barebone->get_option();
+		$default = 'Hello World';
+		$this->assertEqual( $option, $default );
+	}
+
+}
